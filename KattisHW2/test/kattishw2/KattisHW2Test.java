4c7c21f286b65a0b2bbfc9b71d2544e8dc26322e
wstory21 <wbstory@mavs.coloradomesa.edu>
2017-09-26 13:06:34 -0600

    Divisible finished, tests fail

---
+++ KattisHW2/test/kattishw2/KattisHW2Test.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kattishw2;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Warren
+ */
+public class KattisHW2Test {
+    
+    public KattisHW2Test() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of main method, of class KattisHW2.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        KattisHW2.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+    }
+
+    /**
+     * Test of checkDiv method, of class KattisHW2.
+     */
+    @Test
+    public void testCheckDiv() {
+        System.out.println("checkDiv");
+        int visor = 2;
+        int dend = 2;
+        boolean expResult = true;
+        boolean result = KattisHW2.checkDiv(visor, dend);
+        assertEquals(expResult, result);
+    }
+    
+}
