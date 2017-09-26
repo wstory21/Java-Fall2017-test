b7a5f4dd82c6468336ef6b8059b32590b6514299
wstory21 <wbstory@mavs.coloradomesa.edu>
2017-09-26 12:13:39 -0600

    Divisible start

---
+++ KattisHW2/src/kattishw2/KattisHW2.java
@@ -0,0 +1,30 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kattishw2;
+import java.util.Scanner;
+/**
+ *
+ * @author Warren
+ */
+public class KattisHW2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner Scan = new Scanner (System.in);
+        System.out.print("X: ");
+        int x = Scan.nextInt();
+        System.out.print("Y: ");
+        int y = Scan.nextInt();
+        System.out.print("N: ");
+        int n = Scan.nextInt();
+        
+        
+    }
+    
+}
